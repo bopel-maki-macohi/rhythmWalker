80ed12f8f269b203246fa635e9adaf366c775153
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-23 08:39:58 -0500

    save button

--- source/charting/groups/ChartFileTabGroup.hx
+++ source/charting/groups/ChartFileTabGroup.hx
@@ -1,11 +1,22 @@
 package charting.groups;
 
+import flixel.addons.ui.FlxUIButton;
 import editorstuff.TabGroup;
 
 class ChartFileTabGroup extends TabGroup
 {
+	public var saveButton:FlxUIButton;
+
 	override function create()
 	{
 		name = 'File';
+
+		saveButton = new FlxUIButton(10, 20, 'Save', onSave);
+        add(saveButton);
+	}
+
+	function onSave()
+	{
+		ChartingState.song.save();
 	}
 }
