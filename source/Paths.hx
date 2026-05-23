31c81217d5bbeb278846d47f9878a70bf49a2fa3
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-23 07:53:13 -0500

    typo

--- source/Paths.hx
+++ source/Paths.hx
@@ -6,7 +6,7 @@ class Paths
 	public static inline function getAudio(path:String)
 		return getPath('$path.ogg');
 
-	public static inline function getSong(song:String, varation:String = 'default')
+	public static inline function getSong(song:String, variation:String = 'default')
 		return getAudio('${song.toLowerCase()}/${variation.toLowerCase()}');
 
 	public static inline function getImagePath(path:String)
