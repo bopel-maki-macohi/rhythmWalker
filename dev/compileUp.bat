867660fa3a14ec9537577f34591fafb15d6b4bd3
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-25 10:42:40 -0500

    im gonna add a 32bit ver to give them mercy just in case

--- dev/compileUp.bat
+++ dev/compileUp.bat
@@ -1,12 +1,17 @@
 lime build html5
 lime build windows
+lime build windows -32 -D32bit
 
-cd export/release/html5/bin
-wsl 7z a web.zip *
-wsl mv web.zip ../../../
+cd export/64bit/release/html5/bin
+wsl 7z a web-64bit.zip *
+wsl mv web-64bit.zip ../../../
 
-cd ../../windows/bin
-wsl 7z a windows.zip *
-wsl mv windows.zip ../../../
+cd ../../64bit/windows/bin
+wsl 7z a windows-64bit.zip *
+wsl mv windows-64bit.zip ../../../
+
+cd ../../32bit/windows/bin
+wsl 7z a windows-32bit.zip *
+wsl mv windows-32bit.zip ../../../
 
 cd ../../../../
