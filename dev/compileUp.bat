6b5e88fffa1e782839d4746ca7d84d269c5be501
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-25 11:49:49 -0500

    linux is CANCELLED

--- dev/compileUp.bat
+++ dev/compileUp.bat
@@ -1,8 +1,8 @@
 lime build html5
 lime build windows
 lime build windows -32 -D_32bit
-wsl lime build linux
-wsl lime build linux -32 -D_32bit
+@REM wsl lime build linux
+@REM wsl lime build linux -32 -D_32bit
 
 cd export/64bit/release/html5/bin
 wsl 7z a web-64bit.zip *
@@ -12,16 +12,16 @@ cd ../../windows/bin
 wsl 7z a windows-64bit.zip *
 wsl mv windows-64bit.zip ../../../../
 
-cd ../../linux/bin
-wsl 7z a linux-64bit.zip *
-wsl mv linux-64bit.zip ../../../../
+@REM cd ../../linux/bin
+@REM wsl 7z a linux-64bit.zip *
+@REM wsl mv linux-64bit.zip ../../../../
 
 cd ../../../32bit/windows/bin
 wsl 7z a windows-32bit.zip *
 wsl mv windows-32bit.zip ../../../../
 
-cd ../../linux/bin
-wsl 7z a linux-32bit.zip *
-wsl mv linux-32bit.zip ../../../../
+@REM cd ../../linux/bin
+@REM wsl 7z a linux-32bit.zip *
+@REM wsl mv linux-32bit.zip ../../../../
 
 cd ../../../../../
