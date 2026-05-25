889c1ae75919e7bc8f73354ac80a80fcfb9fe000
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-25 11:00:34 -0500

    linux building :D

--- dev/compileUp.bat
+++ dev/compileUp.bat
@@ -1,17 +1,27 @@
 lime build html5
 lime build windows
 lime build windows -32 -D_32bit
+wsl lime build linux
+wsl lime build linux -32 -D_32bit
 
 cd export/64bit/release/html5/bin
 wsl 7z a web-64bit.zip *
-wsl mv web-64bit.zip ../../../
+wsl mv web-64bit.zip ../../../../
 
-cd ../../64bit/windows/bin
+cd ../../windows/bin
 wsl 7z a windows-64bit.zip *
-wsl mv windows-64bit.zip ../../../
+wsl mv windows-64bit.zip ../../../../
 
-cd ../../32bit/windows/bin
+cd ../../linux/bin
+wsl 7z a linux-64bit.zip *
+wsl mv linux-64bit.zip ../../../../
+
+cd ../../../32bit/windows/bin
 wsl 7z a windows-32bit.zip *
-wsl mv windows-32bit.zip ../../../
+wsl mv windows-32bit.zip ../../../../
 
-cd ../../../../
+cd ../../linux/bin
+wsl 7z a linux-32bit.zip *
+wsl mv linux-32bit.zip ../../../../
+
+cd ../../../../../
