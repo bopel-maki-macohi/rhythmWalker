07fed02c5f1fe61b2548c11308971f7358debad3
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-26 17:50:13 -0500

    web support getting cancelled??????????? also 32bit shit should go away, annoying ngl, i can just add an options menu

--- source/util/Flag.hx
+++ source/util/Flag.hx
@@ -6,11 +6,11 @@ enum abstract Flag(Bool) from Bool to Bool
 {
 	public static var list:Map<String, Flag> = [];
 
-	public static final IS_32BIT = new Flag('IS_32BIT', [DefineMacro.isDefined('_32bit'),]);
 	public static final IS_DEBUG = new Flag('IS_DEBUG', [DefineMacro.isDefined('debug'),]);
 
+	public static final PLATFORM_WINDOWS = new Flag('PLATFORM_WINDOWS', [DefineMacro.isDefined('windows')]);
+	public static final PLATFORM_MAC = new Flag('PLATFORM_MAC', [DefineMacro.isDefined('mac')]);
 	public static final PLATFORM_LINUX = new Flag('PLATFORM_LINUX', [DefineMacro.isDefined('linux')]);
-	public static final PLATFORM_WEB = new Flag('PLATFORM_WEB', [DefineMacro.isDefined('web')]);
 	public static final PLATFORM_HASHLINK = new Flag('PLATFORM_HASHLINK', [DefineMacro.isDefined('hl')]);
 
 	public static final SAVE_CLEAR = new Flag('SAVE_CLEAR', [DefineMacro.isDefined('clear') || DefineMacro.isDefined('SAVE_CLEAR')]);
