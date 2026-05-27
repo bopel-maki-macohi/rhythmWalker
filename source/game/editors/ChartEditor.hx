3cb88497a890233169d8af41140e2e53893f1cda
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-26 20:21:46 -0500

    fix grid length and position shit & focus shit

--- source/game/editors/ChartEditor.hx
+++ source/game/editors/ChartEditor.hx
@@ -22,9 +22,11 @@ class ChartEditor extends ConductorState
 	var songLengthInPixels(get, never):Int;
 
 	function get_songLengthInPixels():Int
-		return Std.int(conductor.musicLength * GRID_SIZE);
+		return Std.int((song.audio?.length ?? 1000) * GRID_SIZE);
 
 	var song:Song;
+
+	var songPosition:Float = 0;
 
 	override public function new(?song:Song)
 	{
@@ -70,8 +72,12 @@ class ChartEditor extends ConductorState
 	{
 		super.update(elapsed);
 
-		conductor.update(null);
-		conductor.update(song.audio?.time);
+		if (song.audio?.playing)
+			songPosition += elapsed * 1000;
+		else
+			songPosition = song.audio.time;
+
+		conductor.update(songPosition);
 
 		updateGrid();
 
@@ -83,7 +89,7 @@ class ChartEditor extends ConductorState
 		if (gridTiled == null)
 			return;
 
-		gridTiled.y = (FlxG.height / 2) - conductor.frameMusicPosition;
+		gridTiled.y = (FlxG.height / 2) - songPosition;
 	}
 
 	function controlManagement()
@@ -93,7 +99,25 @@ class ChartEditor extends ConductorState
 			if (song.audio.playing)
 				song.audio.pause();
 			else
-				song.audio.play(song.audio.time == song.audio.length);
+				song.audio.play(songPosition >= song.audio.length);
 		}
+	}
+
+	var wasPlaying:Bool = false;
+
+	override function onFocus()
+	{
+		super.onFocus();
+
+		if (wasPlaying)
+			song.audio.resume();
+	}
+
+	override function onFocusLost()
+	{
+		super.onFocusLost();
+
+		wasPlaying = song.audio.playing;
+		song.audio.pause();
 	}
 }
